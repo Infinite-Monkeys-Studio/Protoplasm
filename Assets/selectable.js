7826977e8b35727cb6d9b3b4676ebac40736480a
Qhenckel <boardman94@gmail.com>
2015-04-18 13:31:57 -0500

    Added selecting

---
+++ Assets/selectable.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var tileSelectionMarker : GameObject;
+
+private var selectorSprite : GameObject;
+
+
+function Start () {
+	selectorSprite = Instantiate (tileSelectionMarker, Vector3(0,0, 0), Quaternion.identity);
+}
+
+function Update () {
+	if(Input.GetMouseButtonDown(0)){
+		var mousePosition : Vector2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		var hitCollider : Collider2D = Physics2D.OverlapPoint(mousePosition);
+
+		Debug.Log("mouse pos "+mousePosition.x+" y "+mousePosition.y+" ");    
+
+
+		if(hitCollider) {
+			selectorSprite.transform.position.x = hitCollider.transform.position.x;
+			selectorSprite.transform.position.y = hitCollider.transform.position.y;  
+			Debug.Log("Hit "+hitCollider.transform.name+" x"+hitCollider.transform.position.x+" y "+hitCollider.transform.position.y);    
+		}
+	}
+}

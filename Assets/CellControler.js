33eac3752896969647e5eda430b71e4f60b42850
Qhenckel <boardman94@gmail.com>
2015-04-18 16:11:57 -0500

    fixed selecting things

--- Assets/CellControler.js
+++ Assets/CellControler.js
@@ -3,12 +3,12 @@
 var tileSelectionMarker : GameObject;
 var baseSpeed : int = 5;
 
-private var selectorSprite : GameObject;
 private var selected : boolean = false;
 private var goalLocation : Vector2;
+var halo : Behaviour;
 
 function Start () {
-	selectorSprite = Instantiate (tileSelectionMarker, Vector3(0,0, 0), Quaternion.identity);
+	halo = GetComponent("Halo");
 }
 
 function Update () {
@@ -16,14 +16,12 @@ function Update () {
 		var mousePosition : Vector2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		var hitCollider : Collider2D = Physics2D.OverlapPoint(mousePosition);
 
-		Debug.Log("mouse pos "+mousePosition.x+" y "+mousePosition.y+" ");    
+		//Debug.Log("mouse pos "+mousePosition.x+" y "+mousePosition.y+" ");
 
 
 		if(hitCollider) {
-			selectorSprite.transform.position.x = hitCollider.transform.position.x;
-			selectorSprite.transform.position.y = hitCollider.transform.position.y;
-			Debug.Log("Hit "+hitCollider.transform.name+" x"+hitCollider.transform.position.x+" y "+hitCollider.transform.position.y);
-			//selected = !selected; 
+			//Debug.Log("Hit "+hitCollider.transform.name+" x"+hitCollider.transform.position.x+" y "+hitCollider.transform.position.y);
+			halo.enabled = ! halo.enabled;
 		}
 	}
 	
@@ -31,8 +29,8 @@ function Update () {
 		goalLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 	}
 	
-	//if(goalLocation != void) {
-	//	transform.LookAt(goalLocation);
-	//	transform.position = transform.forward * baseSpeed;
-	//}
+	if(goalLocation != void) {
+		transform.LookAt(goalLocation);
+		transform.position = transform.forward * baseSpeed;
+	}
 }

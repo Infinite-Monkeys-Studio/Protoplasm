38119a6243b52df2f82200f8ea2e920d7e0902c7
Qhenckel <boardman94@gmail.com>
2015-04-23 22:51:44 -0500

    Working version of algea

--- Assets/ResourceControler.js
+++ Assets/ResourceControler.js
@@ -1,9 +1,23 @@
 ﻿#pragma strict
 
-function Start () {
+var GrowRate: int = 10000; // 1 per x frames
+var ClingDistance : float = 10;
 
+private static var num : int = 0;
+private var coll : Collider2D;
+
+function Start () {
+	coll = GetComponent(Collider2D);
 }
 
 function Update () {
-
+	if(Time.frameCount % GrowRate == 0 && num < 10) {
+		Instantiate(GetComponent("Transform"), Vector2(Random.Range(.1, .2), Random.Range(-.1, .1)) + transform.position, transform.rotation);
+		num++;
+	}
+	
+	var clingto : Collider2D[] = Physics2D.OverlapCircleAll(transform.position, ClingDistance);
+	for(var near: Collider2D in clingto) {
+		transform.position = Vector3.MoveTowards(transform.position, near.transform.position, Time.deltaTime);
+	}
 }
